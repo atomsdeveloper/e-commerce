5007c0b9af8de636f67fc4c7d75b4de1c4c5b114
Renan Nascimento <atomsfrontend@gmail.com>
2024-11-16 20:19:18 -0300

    feat: created categories list component

---
+++ app/_components/category-item.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+// Database
+import { Category } from '@prisma/client';
+
+interface CategoryItemProps {
+  category: Category;
+}
+
+const CategoryItem = ({ category }: CategoryItemProps) => {
+  return (
+    <Link
+      href={`/categories/${category.id}/products`}
+      className="flex items-center justify-center gap-3 rounded-full bg-white px-4 py-3 shadow-md"
+    >
+      <Image
+        src={category.imageUrl}
+        alt={category.name}
+        height={30}
+        width={30}
+      />
+
+      <span className="text-sm font-semibold">{category.name}</span>
+    </Link>
+  );
+};
+
+export default CategoryItem;
