6edc19367d7f821d7e5583c5d47edc44c3196b2e
Renan Nascimento <atomsfrontend@gmail.com>
2024-11-21 20:32:50 -0300

    feat: ensure basic config of files to handle athentication user with google provider

---
+++ app/_provider/auth.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+import { SessionProvider } from 'next-auth/react';
+import { ReactNode } from 'react';
+
+const AuthProvider = ({ children }: { children: ReactNode }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default AuthProvider;
