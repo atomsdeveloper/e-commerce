46cfd96537588ccc940f1d1d5abda2f92d2b0a37
Renan Nascimento <atomsfrontend@gmail.com>
2024-11-24 14:43:57 -0300

    feat: created component cart to create order

---
+++ app/_components/cart.tsx
@@ -0,0 +1,195 @@
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+// HOOKS
+import { useContext, useState } from 'react';
+import { useSession } from 'next-auth/react';
+
+// CONTEXT
+import { CartContext } from '../_context/cart';
+
+// HELPERS
+import { formatPrice } from '../_helpers/price';
+
+// DATABASE
+import { OrderStatus } from '@prisma/client';
+
+// ACTIONS
+import { createOrder } from '../_actions/order';
+
+// ICONS
+import { Loader2 } from 'lucide-react';
+
+// COMPONENTS
+import { Card, CardContent } from './ui/card';
+import { Separator } from './ui/separator';
+import { Button } from './ui/button';
+import CartItem from './cart-item';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from './ui/alert-dialog';
+import { toast } from 'sonner';
+
+interface CartProps {
+  // eslint-disable-next-line no-unused-vars
+  setIsOpen: (isOpen: boolean) => void;
+}
+
+export const Cart = ({ setIsOpen }: CartProps) => {
+  const router = useRouter();
+
+  const [isSubmitLoading, setIsSubmitLoading] = useState(false);
+  const [isConfirmDialogOpen, setIsConfirmDialogOpen] = useState(false);
+
+  const { data } = useSession();
+
+  const { products, subtotalPrice, totalPrice, totalDiscounts, clearCart } =
+    useContext(CartContext);
+
+  const handleFinishOrderClick = async () => {
+    if (!data?.user) return;
+
+    const restaurant = products[0].restaurant;
+
+    try {
+      setIsSubmitLoading(true);
+
+      await createOrder({
+        subtotalPrice,
+        totalDiscounts,
+        totalPrice,
+        deliveryFee: restaurant.deliveryFee,
+        deliveryTimeMinutes: restaurant.deliveryTimeMinutes,
+        restaurant: {
+          connect: { id: restaurant.id },
+        },
+        status: OrderStatus.CONFIRMED,
+        user: {
+          connect: { id: data.user.id },
+        },
+        products: {
+          createMany: {
+            data: products.map((product) => ({
+              productId: product.id,
+              quantity: product.quantity,
+            })),
+          },
+        },
+      });
+
+      clearCart();
+      setIsOpen(false);
+
+      toast('Pedido finalizado com sucesso!', {
+        description: 'Você pode acompanhá-lo na tela dos seus pedidos.',
+        action: {
+          label: 'Meus Pedidos',
+          onClick: () => router.push('/my-orders'),
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsSubmitLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex h-full flex-col py-5">
+        {products.length > 0 ? (
+          <>
+            <div className="flex-auto space-y-4">
+              {products.map((product) => (
+                <CartItem key={product.id} cartProduct={product} />
+              ))}
+            </div>
+
+            {/* TOTAIS */}
+            <div className="mt-6">
+              <Card>
+                <CardContent className="space-y-2 p-5">
+                  <div className="flex items-center justify-between text-xs">
+                    <span className="text-muted-foreground">Subtotal</span>
+                    <span>{formatPrice(subtotalPrice)}</span>
+                  </div>
+
+                  <Separator />
+
+                  <div className="flex items-center justify-between text-xs">
+                    <span className="text-muted-foreground">Descontos</span>
+                    <span>- {formatPrice(totalDiscounts)}</span>
+                  </div>
+
+                  <Separator className="h-[0.5px]" />
+
+                  <div className="flex items-center justify-between text-xs">
+                    <span className="text-muted-foreground">Entrega</span>
+
+                    {Number(products?.[0].restaurant.deliveryFee) === 0 ? (
+                      <span className="uppercase text-primary">Grátis</span>
+                    ) : (
+                      formatPrice(Number(products?.[0].restaurant.deliveryFee))
+                    )}
+                  </div>
+
+                  <Separator />
+
+                  <div className="flex items-center justify-between text-xs font-semibold">
+                    <span>Total</span>
+                    <span>{formatPrice(totalPrice)}</span>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+
+            {/* FINALIZAR PEDIDO */}
+            <Button
+              className="mt-6 w-full"
+              onClick={() => setIsConfirmDialogOpen(true)}
+              disabled={isSubmitLoading}
+            >
+              Finalizar pedido
+            </Button>
+          </>
+        ) : (
+          <h2 className="text-left font-medium">Sua sacola está vazia.</h2>
+        )}
+      </div>
+
+      <AlertDialog
+        open={isConfirmDialogOpen}
+        onOpenChange={setIsConfirmDialogOpen}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Deseja finalizar seu pedido?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Ao finalizar seu pedido, você concorda com os termos e condições
+              da nossa plataforma.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={handleFinishOrderClick}
+              disabled={isSubmitLoading}
+            >
+              {isSubmitLoading && (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Finalizar
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+};
